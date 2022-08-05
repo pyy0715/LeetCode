2bb16ec9e58b6233443cd033ba002c1107028477
pyy0715 <47301926+pyy0715@users.noreply.github.com>
2022-08-06 01:50:27 +0900

    Time: 43 ms (35.90%), Space: 13.4 MB (98.72%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -1,12 +1,26 @@
+import collections
+
 class Solution(object):
     def removeDuplicateLetters(self, s):
         """
         :type s: str
         :rtype: str
         """
-        for char in sorted(set(s)):
-            suffix = s[s.index(char):]
-            if set(s)==set(suffix):
-                return char + self.removeDuplicateLetters(suffix.replace(char, ''))
-        return ''
+        counter, stack = collections.Counter(s), []
+        
+        for char in s:
+            counter[char]-=1
+            
+            if char in stack:
+                continue
+            
+            while stack and char<stack[-1] and counter[stack[-1]]>0:
+                stack.pop()
+                
+            stack.append(char)
+            
+        return ''.join(stack)
+                
+            
+        
         
