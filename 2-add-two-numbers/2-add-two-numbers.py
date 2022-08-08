7076702958f1379151dec84d36cae75d087e3108
pyy0715 <47301926+pyy0715@users.noreply.github.com>
2022-08-08 17:00:42 +0900

    Time: 79 ms (61.44%), Space: 13.6 MB (43.37%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        arr = []
+        head = ListNode(0)
+        
+        carry = 0
+        while l1 or l2 or carry:
+            sum = 0
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+                
+            carry, val = divmod(sum+carry, 10)
+            head.next = ListNode(val)
+            
+            arr.append(head)
+            head = head.next
+            
+        return arr[0].next
+            
+            
+            
+            
+        
