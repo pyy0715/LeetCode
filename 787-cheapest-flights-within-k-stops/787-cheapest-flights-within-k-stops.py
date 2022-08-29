73f848fdc04603bd2538ca7195a5f39a85e71321
pyy0715 <47301926+pyy0715@users.noreply.github.com>
2022-08-29 15:16:32 +0900

    Time: 111 ms (83.74%), Space: 15.4 MB (11.58%) - LeetHub

---
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,35 @@
+import collections
+import heapq, sys
+
+class Solution(object):
+    def findCheapestPrice(self, n, flights, src, dst, K):
+        """
+        :type n: int
+        :type flights: List[List[int]]
+        :type src: int
+        :type dst: int
+        :type k: int
+        :rtype: int
+        """
+        graph = collections.defaultdict(list)
+        weight = [(sys.maxsize, K)] * n
+        # 그래프 인접 리스트 구성
+        for u, v, w in flights:
+            graph[u].append((v, w))
+
+        # 큐 변수: [(가격, 정점, 남은 가능 경유지 수)]
+        Q = [(0, src, K)]
+
+        # 우선 순위 큐 최소값 기준으로 도착점까지 최소 비용 판별
+        while Q:
+            price, node, k = heapq.heappop(Q)
+            if node == dst:
+                return price
+            if k >= 0:
+                for v, w in graph[node]:
+                    alt = price + w
+                    if alt < weight[v][0] or k-1 >= weight[v][1]:
+                        weight[v] = (alt, k-1)
+                        heapq.heappush(Q, (alt, v, k - 1))
+        return -1
+        
